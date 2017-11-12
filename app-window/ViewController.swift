0f31308155d25a10e9ee33601531c0ed070d2887
Егор Гурьев <egorgurev@MacBook-Air-Egor.local>
2017-11-12 17:47:30 +0900

    first commit

---
+++ app-window/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  app-window
+//
+//  Created by Егор Гурьев on 12.11.17.
+//  Copyright © 2017 Егор Гурьев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
